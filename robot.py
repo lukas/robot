413809dee852b5d6916b28f83b9b7600319e867e
lukas <lukeb@cs.stanford.edu>
2016-12-27 23:21:09 +0000

    Substitute wheels for drive

--- robot.py
+++ robot.py
@@ -31,32 +31,32 @@ if (len(sys.argv) == 1):
 cmd = sys.argv[1]
 	
 if (cmd == 'forward'):
-	drive.forward(200, 1)
+	wheels.forward(200, 1)
 
 elif (cmd == 'backward'):
-	drive.backward(200, 1)
+	wheels.backward(200, 1)
 
 elif (cmd == 'left'):
-	drive.left(200, 1)
+	wheels.left(200, 1)
 
 elif (cmd == 'right'):
-	drive.right(200, 1)
+	wheels.right(200, 1)
 
 elif (cmd == 'wheel1'):
-	drive.spinMotor(1, 200, 1)
+	wheels.spinMotor(1, 200, 1)
 
 elif (cmd == 'wheel2'):
-	drive.spinMotor(2, 200, 1)
+	wheels.spinMotor(2, 200, 1)
 
 elif (cmd == 'wheel3'):
-	drive.spinMotor(3, 200, 1)
+	wheels.spinMotor(3, 200, 1)
 
 elif (cmd == 'wheel4'):
-	drive.spinMotor(4, 200, 1)
+	wheels.spinMotor(4, 200, 1)
 
 elif (cmd == 'shake'):
-	drive.left(200, 0.25)
-	drive.right(200, 0.25)
+	wheels.left(200, 0.25)
+	wheels.right(200, 0.25)
 
 else:
 	print usage
